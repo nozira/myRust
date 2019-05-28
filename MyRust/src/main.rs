01ac731a988e20c31eb6108f13196472e6263295
nozira <nozira.net@gmail.com>
2019-05-28 23:43:06 +0900

    new branch develop

--- MyRust/src/main.rs
+++ MyRust/src/main.rs
@@ -1,3 +1,33 @@
+#![allow(unused_variables,non_snake_case)]
 fn main() {
     println!("Hello, world!");
+
+    //変数の束縛
+    let a = 5;
+    println!("{}",a);
+
+    //パターン
+    let (x,y) = (1,2);
+    println!("({},{})",x,y);
+
+    //型アノテーション
+    let b:i32 = 6;
+    println!("{}",b);
+
+    //可変性
+    let mut c = 8;
+    println!("{}",c);
+    c = 7;
+    println!("{}",c);
+
+    //スコープとシャドーイング
+    let d:i32 = 17;
+    {
+        println!("{}",d);
+        let d:i32 = 3;
+        println!("{}",d);
+    }
+    println!("{}",d);
+    let d = 42;
+    println!("{}",d);
 }
